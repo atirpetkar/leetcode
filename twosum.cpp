782c6ba3c765e7fd3d8c2c07b85c14ed82792f32
atirpetkar <ap8185@g.rit.edu>
2018-04-25 00:04:43 -0400

    Two Sum cpp solution using hashmap

---
+++ twosum.cpp
@@ -0,0 +1,31 @@
+/*
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ 
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ 
+ Example:
+ 
+ Given nums = [2, 7, 11, 15], target = 9,
+ 
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ 
+ */
+
+
+#include <cmath>
+
+class Solution {
+	public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            
+            map<int, int> hm;
+            for (int i = 0; i < nums.size() ; i++) hm[nums[i]] = i;
+
+            for (int i = 0; i < nums.size() ; i++)
+            if ((hm.find(target - nums[i]) != hm.end()) &&
+                (hm[target - nums[i]] != i)) //check if it is not pointing to same index
+                return vector<int> {i, hm[target - nums[i]]};
+
+        }
+};
